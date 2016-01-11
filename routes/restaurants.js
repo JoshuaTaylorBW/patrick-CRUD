a0a87ae5c9d7197691815ac2f6ee15317482d322
Patrick Beam <patrick.j.beam@gmail.com>
2016-01-11 15:35:04 -0700

    added a bunch of forward slashes

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 var database = require('../database');
 
-router.get('/restaurants', function(req, res, next) {
+router.get('/restaurants/', function(req, res, next) {
     database.outputAll().then(function(result) {
         res.render('restaurants/index', {restaurants: result});
     })
 });
 
-router.get('/restaurants/all', function(req, res, next) {
+router.get('/restaurants/all/', function(req, res, next) {
     database.outputAll().then(function(result) {
         res.render('restaurants/all', {restaurants: result});
     })
 });
 
-router.get('/restaurants/new', function(req, res, next) {
+router.get('/restaurants/new/', function(req, res, next) {
   res.render('restaurants/new');
 });
 
-router.post('/restaurants', function(req, res, next) {
+router.post('/restaurants/', function(req, res, next) {
     var name = req.body.name;
     var location = req.body.location;
     var state = req.body.state;
@@ -31,25 +31,25 @@ router.post('/restaurants', function(req, res, next) {
 }
     var bio = req.body.bio;
     database.addPlace(name, location, state, cuisine, rating, image, bio).then(function(result){
-        res.redirect('/restaurants/all')
+        res.redirect('/restaurants/all/')
     })
 })
 
-router.get('/restaurants/:name', function (req, res, next) {
+router.get('/restaurants/:name/', function (req, res, next) {
     var name = req.params.name;
     database.outputByName(name).then(function(result) {
         res.render('restaurants/show', {restaurant: result[0]})
     })
 })
 
-router.get('/restaurants/:name/edit', function (req, res, next) {
+router.get('/restaurants/:name/edit/', function (req, res, next) {
     var name = req.params.name;
     database.outputByName(name).then(function(result) {
         res.render('restaurants/edit', {restaurant: result[0]})
     })
 })
 
-router.post('/restaurants/:name', function (req, res, next) {
+router.post('/restaurants/:name/', function (req, res, next) {
     var x = req.params.name;
     var name = req.body.name;
     var location = req.body.location;
@@ -67,10 +67,10 @@ router.post('/restaurants/:name', function (req, res, next) {
     })
 })
 
-router.post('/restaurants/del/:name', function (req, res, next) {
+router.post('/restaurants/del/:name/', function (req, res, next) {
     var name = req.params.name;
     database.deletePlace(name).then(function(result) {
-        res.redirect('/restaurants/all');
+        res.redirect('/restaurants/all/');
     })
 })
 
